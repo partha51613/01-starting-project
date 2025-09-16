cde3da61fb38cb99b5bc7cc4f7b30817e3fc5613
Partha <github@partha51613.com>
2025-09-17 00:59:51 +0530

    vid 27 | using signals

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 
@@ -7,26 +7,31 @@ import { DUMMY_USERS } from '../dummy-users';
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
-  randomIndex = Math.floor(Math.random()*DUMMY_USERS.length)
-  selectedUser = DUMMY_USERS[this.randomIndex];
-  
-  get imagePath(){ //Because of the 'get' keyword, it acts like a property
-    return '/assets/users/' + this.selectedUser.avatar;
+  randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+  // selectedUser = DUMMY_USERS[this.randomIndex] ; //old way
+  selectedUser = signal(DUMMY_USERS[this.randomIndex]); //signal way
+  imagePath = computed(() => {
+    return '/assets/users/' + this.selectedUser().avatar;
+  });
+
+
+
+  // imagePath = computed(() => '/assets/users/' + this.selectedUser().avatar);
+
+  // get imagePath(){ //Because of the 'get' keyword, it acts like a property
+  //   return '/assets/users/' + selectedUser().get(imagePath);
+  // }
+
+  changeRandomIndex() {
+    this.randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   }
 
-
-  changeRandomIndex(){
-    this.randomIndex = Math.floor(Math.random()*DUMMY_USERS.length)
+  onSelectUser() {
+    this.changeRandomIndex();
+    // this.selectedUser = DUMMY_USERS[this.randomIndex];
+    this.selectedUser.set(DUMMY_USERS[this.randomIndex]);
   }
-
-  onSelectUser(){
-    console.log("test");
-    this.changeRandomIndex()
-    this.selectedUser = DUMMY_USERS[this.randomIndex];
-    
-  }
-
 }
