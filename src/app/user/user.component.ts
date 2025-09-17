8c5512f958de5906643678822aa47359a7edc6c4
Partha <github@partha51613.com>
2025-09-17 22:18:53 +0530

    vid30 done

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
+import { Component, Input } from '@angular/core';
 
 
 @Component({
@@ -10,28 +9,28 @@ import { DUMMY_USERS } from '../dummy-users';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-  // selectedUser = DUMMY_USERS[this.randomIndex] ; //old way
-  selectedUser = signal(DUMMY_USERS[this.randomIndex]); //signal way
-  imagePath = computed(() => {
-    return '/assets/users/' + this.selectedUser().avatar;
-  });
 
+  @Input() avatar!: string;
+  @Input() name!: string;
 
-
-  // imagePath = computed(() => '/assets/users/' + this.selectedUser().avatar);
-
-  // get imagePath(){ //Because of the 'get' keyword, it acts like a property
-  //   return '/assets/users/' + selectedUser().get(imagePath);
-  // }
-
-  changeRandomIndex() {
-    this.randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+  get imagePath(){
+    return '/assets/users/'+ this.avatar;
   }
 
   onSelectUser() {
-    this.changeRandomIndex();
-    // this.selectedUser = DUMMY_USERS[this.randomIndex];
-    this.selectedUser.set(DUMMY_USERS[this.randomIndex]);
+
   }
 }
+ 
+
+
+
+
+
+
+
+/* OLD CODE */
+
+  // imagePath = computed(()=>{
+  //   return '/assets/users/'+ this.avatar;
+  // }) //This also works
