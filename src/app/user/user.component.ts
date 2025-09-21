44933a15f426a3903eb9a5a61599c0460b6c7fb4
Partha <github@partha51613.com>
2025-09-21 15:10:20 +0530

    @Output used | event emitter

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, input, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 
 @Component({
@@ -9,16 +9,17 @@ import { Component, computed, input, Input } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-
+  @Input({required: true}) id!: string;
   @Input({required: true}) name!:string 
   @Input({required: true}) avatar!:string 
+  @Output() select = new EventEmitter();
 
   get imagePath(){
     return '/assets/users/' + this.avatar;
   }
 
   onSelectUser() {
-
+    this.select.emit(this.id);
   }
 }
  
