a68e9ac5d0b947840c587227fd7497b6c149dd76
Partha <partha.choudhury@rnd.iitg.ac.in>
2025-09-24 15:38:41 +0530

    vid 46

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+interface Task {
+      id: string,
+      userId: string,
+      title: string,
+      summary: string
+      dueDate: string
+}
+
 
 @Component({
   selector: 'app-task',
@@ -7,6 +16,9 @@ import { Component } from '@angular/core';
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
-export class TaskComponent {
 
+
+
+export class TaskComponent {
+  @Input({required: true}) task!: Task;
 }
