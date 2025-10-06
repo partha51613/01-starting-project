555df0427f2b14742e7b5948b4b0a743df24d9bb
Partha <github@partha51613.com>
2025-10-06 07:06:34 +0530

    vid 49

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -42,4 +42,7 @@ export class TasksComponent {
     return this.dummyTasks.filter((task) => task.userId === this.userId)
   }
 
+  onCompleteTask(id: string){
+    this.dummyTasks = this.dummyTasks.filter((task) => task.id !== id);
+  }
 }
